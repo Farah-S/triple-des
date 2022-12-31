99cb8297a661d4d57c460216ab3c03080cc08120
Farah Sherif <farah.faroha132@gmail.com>
2022-12-31 21:39:45 +0200

    fixes and ibrahim's code

--- project.py
+++ project.py
@@ -1,63 +1,80 @@
-import operator
 # variables
 key1 = 'A6F42BD15C61F4C9'
 key2 = 'FB36A2C73D2C90E5'
 key3 = '2E4C65A2E836B73A'
 
-s_box1 = [[1, 14, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-          [0, 5, 7, 14, 4, 12, 13, 1, 10, 6, 2, 11, 9, 15, 3, 8],
-          [4, 1, 14, 8, 3, 6, 2, 11, 15, 12, 9, 7, 13, 0, 5, 10],
-          [15, 2, 8, 12, 4, 9, 1, 7, 5, 11, 13, 14, 0, 10, 6, 3]]
+s_box1 = [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+         [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+         [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+         [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]]
 
-s_box2 = [[5, 11, 8, 14, 6, 1, 13, 4, 9, 7, 12, 3, 2, 10, 15, 0],
-          [13, 3, 14, 7, 15, 12, 8, 4, 2, 0, 10, 11, 6, 9, 1, 5],
-          [10, 4, 7, 1, 0, 14, 13, 11, 15, 8, 12, 9, 6, 3, 5, 2],
-          [3, 8, 0, 11, 13, 15, 4, 12, 1, 6, 7, 2, 10, 5, 14, 9]]
+s_box2 = [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+         [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+         [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+         [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]]
 
-s_box3 = [[0, 10, 9, 4, 6, 13, 15, 5, 11, 3, 2, 7, 1, 14, 12, 8],
-          [3, 7, 1, 9, 13, 14, 6, 11, 12, 8, 5, 4, 2, 10, 15, 0],
-          [10, 6, 14, 9, 8, 5, 3, 0, 11, 1, 12, 2, 15, 13, 4, 7],
-          [11, 0, 3, 10, 6, 9, 8, 7, 14, 15, 4, 13, 1, 5, 12, 2]]
+s_box3 = [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+         [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+         [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+         [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]]
 
-s_box4 = [[7, 3, 4, 13, 0, 6, 9, 1, 10, 12, 8, 5, 11, 2, 14, 15],
-          [3, 8, 1, 15, 6, 5, 0, 13, 14, 7, 12, 2, 10, 11, 4, 9],
-          [0, 6, 9, 10, 2, 1, 7, 3, 5, 11, 13, 14, 15, 12, 8, 4],
-          [13, 5, 10, 6, 1, 0, 3, 8, 9, 4, 15, 11, 2, 7, 12, 14]]
+s_box4 = [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+         [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+         [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+         [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]]
 
-s_box5 = [[12, 2, 14, 10, 7, 11, 1, 6, 8, 15, 13, 5, 3, 0, 4, 9],
-          [4, 1, 12, 2, 14, 7, 3, 11, 15, 10, 5, 0, 13, 9, 8, 6],
-          [14, 12, 10, 1, 11, 3, 7, 8, 5, 9, 2, 15, 6, 13, 0, 4],
-          [1, 8, 2, 7, 11, 4, 12, 3, 6, 5, 10, 9, 0, 14, 15, 13]]
+s_box5 = [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+         [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+         [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+         [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]]
 
-s_box6 = [[2, 10, 11, 5, 9, 12, 6, 8, 0, 3, 13, 14, 4, 7, 15, 1],
-          [0, 5, 14, 12, 7, 2, 9, 15, 6, 11, 3, 4, 10, 1, 13, 8],
-          [9, 4, 5, 15, 12, 8, 2, 13, 7, 10, 14, 0, 11, 3, 1, 6],
-          [14, 13, 12, 2, 9, 15, 5, 0, 1, 4, 11, 7, 6, 10, 8, 3]]
+s_box6 = [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+         [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+         [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+         [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]]
 
-s_box7 = [[14, 1, 12, 4, 5, 10, 8, 3, 13, 2, 9, 7, 15, 0, 6, 11],
-          [3, 10, 1, 7, 14, 9, 11, 0, 4, 13, 15, 2, 12, 5, 8, 6],
-          [11, 14, 1, 3, 2, 13, 7, 4, 0, 5, 6, 8, 10, 15, 9, 12],
-          [6, 1, 3, 8, 11, 14, 0, 7, 9, 15, 10, 5, 4, 12, 13, 2]]
+s_box7 = [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+         [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+         [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+         [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]]
 
-s_box8 = [[3, 12, 8, 14, 6, 5, 1, 11, 0, 9, 13, 4, 15, 10, 2, 7],
-          [11, 5, 3, 8, 0, 13, 7, 14, 2, 15, 6, 1, 10, 4, 9, 12],
-          [7, 1, 14, 11, 9, 2, 4, 12, 10, 6, 0, 3, 5, 13, 15, 8],
-          [12, 11, 4, 7, 14, 0, 8, 3, 5, 2, 9, 10, 13, 15, 6, 1]]
+s_box8 = [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+         [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+         [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+         [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]
 
 s_boxes = [s_box1, s_box2, s_box3, s_box4, s_box5, s_box6, s_box7, s_box8]
 
-initial_perm = [11, 58, 33, 12, 45, 1, 20, 18, 13, 38, 6, 29, 9, 22, 64, 60, 21, 8, 36, 41, 17, 14, 54, 42, 34, 30, 49, 53, 56, 26,
-                61, 44, 50, 62, 52, 47, 27, 10, 39, 28, 7, 24, 57, 32, 5, 63, 19, 55, 30, 3, 59, 35, 40, 23, 48, 4, 43, 2, 51, 16, 31, 25, 46, 15]
-
-expansion_perm = [32, 1, 2, 3, 4, 5, 6, 7, 6, 7, 8, 9, 10, 11, 10, 11, 12, 13, 14, 13, 14, 15, 16,
-                  17, 18, 17, 18, 19, 20, 21, 22, 21, 22, 23, 24, 25, 26, 25, 26, 27, 28, 29, 30, 29, 30, 31, 32, 11]
-
-straight_perm = [6, 17, 2, 12, 28, 21, 29, 7, 1, 5, 31, 26, 15, 18,
-                 23, 11, 20, 8, 25, 4, 32, 30, 3, 19, 9, 13, 27, 16, 22, 10, 14, 24]
-
-final_perm = [4, 48, 8, 56, 16, 64, 24, 31, 63, 47, 7, 55, 5, 32, 39, 23, 30, 46, 6, 54, 14, 62, 22, 38, 13, 53, 29, 37, 5, 21, 61,
-              45, 28, 44, 40, 20, 60, 12, 52, 36, 3, 35, 43, 11, 59, 27, 51, 19, 42, 2, 34, 50, 10, 58, 18, 26, 33, 41, 1, 49, 9, 57, 17, 25]
-
+initial_perm = [58, 50, 42, 34, 26, 18, 10, 2,
+                60, 52, 44, 36, 28, 20, 12, 4,
+                62, 54, 46, 38, 30, 22, 14, 6,
+                64, 56, 48, 40, 32, 24, 16, 8,
+                57, 49, 41, 33, 25, 17, 9, 1,
+                59, 51, 43, 35, 27, 19, 11, 3,
+                61, 53, 45, 37, 29, 21, 13, 5,
+                63, 55, 47, 39, 31, 23, 15, 7]
+expansion_perm = [32, 1, 2, 3, 4, 5, 4, 5,
+         6, 7, 8, 9, 8, 9, 10, 11,
+         12, 13, 12, 13, 14, 15, 16, 17,
+         16, 17, 18, 19, 20, 21, 20, 21,
+         22, 23, 24, 25, 24, 25, 26, 27,
+         28, 29, 28, 29, 30, 31, 32, 1]
+straight_perm = [16,  7, 20, 21,
+       29, 12, 28, 17,
+       1, 15, 23, 26,
+       5, 18, 31, 10,
+       2,  8, 24, 14,
+       32, 27,  3,  9,
+       19, 13, 30,  6,
+       22, 11,  4, 25]
+final_perm = [40, 8, 48, 16, 56, 24, 64, 32,
+              39, 7, 47, 15, 55, 23, 63, 31,
+              38, 6, 46, 14, 54, 22, 62, 30,
+              37, 5, 45, 13, 53, 21, 61, 29,
+              36, 4, 44, 12, 52, 20, 60, 28,
+              35, 3, 43, 11, 51, 19, 59, 27,
+              34, 2, 42, 10, 50, 18, 58, 26,
+              33, 1, 41, 9, 49, 17, 57, 25]
 key_parity = [17, 41, 49, 34, 25, 57, 9,
               18, 50, 58, 26, 33, 42, 1,
               51, 27, 59, 10, 35, 3, 2,
@@ -75,11 +92,9 @@ key_compression = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26
 # ---------------------------------------------------------------------
 
 # key generation for the 16 rounds
-
-
 def generateKeys(key):
     roundKeys = []
-    binKey = stringToBinray(key)
+    binKey = stringToBinary(key)
 
     # perform parity drop
     key56bit = dropKeyParity(binKey)
@@ -131,8 +146,6 @@ def compressKey(key):
 # ---------------------------------------------------------------------
 
 # initial permutation
-
-
 def initialPermutation(plain):
     return ''
 
@@ -145,10 +158,10 @@ def expansionPermutation(text):
 # XOR with key
 def XOR(value1, value2):
     a_lenght=len(value1)
-    b_lenght=len(value1)
+    b_lenght=len(value2)
     c_original_lenght=max(a_lenght,b_lenght)
 
-    c = int(value1,2) ^ int(value1,2)
+    c = int(value1,2) ^ int(value2,2)
     c='{0:b}'.format(c)
     c=str(c)
     while (len(c)<c_original_lenght):
@@ -183,7 +196,7 @@ def SBox(text):
         row_and_column_intersection=str(row_and_column_intersection)
         while(len(row_and_column_intersection)<4):
             row_and_column_intersection="0"+row_and_column_intersection
-        row_and_column_intersection=bin(row_and_column_intersection).replace("0b", "")
+        # row_and_column_intersection=bin(row_and_column_intersection).replace("0b", "")
         
         final_s_box_output += row_and_column_intersection
         j += 1
@@ -193,34 +206,36 @@ def SBox(text):
 
 # straight permutation
 def straightPermutation(text):
-    return ''
+    y=""
+    for i in range(len(straight_perm)):
+        y+=text[straight_perm[i]-1]
+    return y
 
 
 # swap
 def swap(text):
-    return ''
+    i = int(len(text)/2)
+    return (text[i:] + text[:i])
 
 
 # final permutation
 def finalPermutation(text):
-    return ''
+    y=""
+    for i in range(len(final_perm)):
+        y=y+text[(final_perm[i]-1)]
+    return y
 
 # ---------------------------------------------------------------------
 
 # takes input string and transforms it into binary
 
 
-def stringToBinray(string):
+def toString(binaryString):
+    return "".join([chr(int(binaryString[i:i+8],2)) for i in range(0,len(binaryString),8)])
+
+
+def stringToBinary(string):
     return ''.join(format(i, '08b') for i in bytearray(string, encoding='utf-8'))
-
-
-# takes binary and transforms it into string
-def binaryToString(binary):
-    # convert binary to int
-    intString = int(binary, 2)
-    # convert int to hexadecimal
-    hexaString = format(intString, 'x')
-    return (hexaString)
 
 
 # splits the string into 64 bit blocks
@@ -228,7 +243,7 @@ def divideToBlocks(bin):
     length = len(bin)
     if length % 64 != 0:
         for j in range((64 - length) % 64):
-            bin = bin + '0'
+            bin = '0'+bin 
         length = len(bin)
     blocks = []
     for i in range(0, length, 64):
@@ -244,16 +259,26 @@ def divideToBlocks(bin):
 
 def DESencrypt(keys, block):
     afterIP = initialPermutation(block)
-    left = afterIP[:28]
-    right = afterIP[28:]
+    left = afterIP[:int(len(afterIP)/2)]
+
+    right = afterIP[int(len(afterIP)/2):]
+    
     for i in range(16):
+        # print(len(right))
         expanded = expansionPermutation(right)
+        # print(len(expanded))
         XORed = XOR(expanded, keys[i])
         afterSBox = SBox(XORed)
+        # print(afterSBox)
         perm = straightPermutation(afterSBox)
         leftXright = XOR(left, perm)
+        # print('ddddddddddddddddddd')
+        # print(len(right))
+        # print(len(leftXright))
         left = right
         right = leftXright
+       
+        # print("round "+str(i+1)+": " +toString(left+right))
     afterRounds = left + right
     swapped = swap(afterRounds)
     cipher = finalPermutation(swapped)
@@ -262,51 +287,74 @@ def DESencrypt(keys, block):
 
 
 def DESdecrypt(keys, block):
-    roundKeys = keys[::-1]
-    return DESencrypt(roundKeys, block)
+    roundsK = keys[::-1]
+    return DESencrypt(roundsK, block)
 
 # ---------------------------------------------------------------------
 
 # encyrption and decryption of triple DES
 
 
-def tripleDESencrypt(plainBlocks):
-    cipherBinary = ''
+def tripleDESencrypt(blocks):
+    cipherBin = ''
     key1rounds = generateKeys(key1)
     key2rounds = generateKeys(key2)
     key3rounds = generateKeys(key3)
 
-    for i in range(len(plainBlocks)):
-        enc1 = DESencrypt(key1rounds, plainBlocks[i])
+    for i in range(len(blocks)):
+        print("encrypt")
+        enc1 = DESencrypt(key1rounds, blocks[i])
+        print("decrypt")
         dec = DESdecrypt(key2rounds, enc1)
+        print("encrypt")
         enc2 = DESencrypt(key3rounds, dec)
-        cipherBinary = cipherBinary + enc2
-    return cipherBinary
+        cipherBin = cipherBin + enc2
+    return cipherBin
 
 
-def tripleDESdecrypt(cipherBlocks):
-    plainBinary = ''
+def tripleDESdecrypt(blocks):
+    plainBin = ''
     key1rounds = generateKeys(key1)
     key2rounds = generateKeys(key2)
     key3rounds = generateKeys(key3)
 
-    for i in range(len(cipherBlocks)):
-        dec1 = DESdecrypt(key3rounds, cipherBlocks[i])
+    for i in range(len(blocks)):
+        dec1 = DESdecrypt(key3rounds, blocks[i])
         enc = DESencrypt(key2rounds, dec1)
         dec2 = DESdecrypt(key1rounds, enc)
-        plainBinary = plainBinary + dec2
-    return plainBinary
+        plainBin = plainBin + dec2
+    return plainBin
 
 # ---------------------------------------------------------------------
 
 
+# inputString = input('Enter plain text:')
+# binary = stringToBinary(inputString)
+# # encrypt
+# plainBlocks = divideToBlocks(binary)
+# cipherBinary = tripleDESencrypt(plainBlocks)
+# cipherText = toString(cipherBinary)
+# # decrypt
+# cipherBlocks = divideToBlocks(binary)
+# plainBinary = tripleDESdecrypt(cipherBlocks)
+# plainText = toString(plainBinary)
+
+
+
+
 inputString = input('Enter plain text:')
-binary = stringToBinray(inputString)
+# print(originalbinary)
 # encrypt
-plainBlocks = divideToBlocks(binary)
+originalbinary = stringToBinary(inputString)
+plainBlocks = divideToBlocks((originalbinary))
+# print(plainBlocks)
 cipherBinary = tripleDESencrypt(plainBlocks)
-cipherText = binaryToString(cipherBinary)
+cipherText = toString(cipherBinary)
+print(cipherText)
+
 # decrypt
-cipherBlocks = divideToBlocks(binary)
+cipherBlocks = divideToBlocks(cipherBinary)
 plainBinary = tripleDESdecrypt(cipherBlocks)
-plainText = binaryToString(plainBinary)
+plainText = toString(plainBinary)
+# print(plainBinary)
+print( plainText)
